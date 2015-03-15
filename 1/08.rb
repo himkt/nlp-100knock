badfc9c08067ed719e1b3fa9faee329a2ac2be29
Makoto Hiramatsu <s1311536@u.tsukuba.ac.jp>
2015-03-15 15:02:06 +0900

    08(not completed)

--- 1/08.rb
+++ 1/08.rb
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 
-def chipper(str)
+def chipher(str)
+  chiphed = ''
+  str.split(//).each do |char|
+    if char.ord >= 97 && char.ord <= 122
+      chiphed += (219 - char.ord).chr('UTF-8')
+    else
+      chiphed += char
+    end
+  end
+  return chiphed
 end
+
+str = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+
+p chipher(str)
