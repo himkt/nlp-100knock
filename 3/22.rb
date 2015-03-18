4ccdea25b16cde9ceb5970993b263cec2a2822db
Makoto Hiramatsu <s1311536@u.tsukuba.ac.jp>
2015-03-18 20:12:43 +0900

    add 22

--- 3/22.rb
+++ 3/22.rb
@@ -4,7 +4,7 @@ open('../data/uk.json', 'r') do |input|
   while line = input.gets
     line.split('\n').each do |item|
       if item =~ /Category/
-        p item
+        p item.match(/Category:(.*)(?:|\*)(?:\]\])/)[1]
       end
     end
   end
