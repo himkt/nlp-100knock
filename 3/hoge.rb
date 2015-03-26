d95c3a9ac3bee3a5b00ff27c3ae3ce8710eb176a
Makoto Hiramatsu <s1311536@u.tsukuba.ac.jp>
2015-03-26 20:38:31 +0900

    24.rb

--- 3/hoge.rb
+++ 3/hoge.rb
@@ -3,9 +3,7 @@
 open('../data/uk.json', 'r') do |input|
   while line = input.gets
     line.split('\n').each do |item|
-      if match = item.match(/(File:.*?)\|/)
-        puts match[1]
-      end
+      puts item
     end
   end
 end
