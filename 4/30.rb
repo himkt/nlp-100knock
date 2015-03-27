b49294f35c53c0cff1d910e7a030a83cb9b48fa5
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-03-27 22:29:50 +0900

    add 30 but have not solved yet

--- 4/30.rb
+++ 4/30.rb
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 require 'natto'
+
+open('../data/neko.txt.mecab', 'r') do |input|
+  10.times do
+    line = input.gets
+    next if line =~ /EOS/
+    p line.chomp.split(/\t/)
+  end
+end
