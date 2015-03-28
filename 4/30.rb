8e873408f5b6430d55df4660a6c4e3380de1f559
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-03-28 17:09:45 +0900

    update 30

--- 4/30.rb
+++ 4/30.rb
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 require 'natto'
 
+# EOSまでで一行
 open('../data/neko.txt.mecab', 'r') do |input|
-  10.times do
-    line = input.gets
-    next if line =~ /EOS/
-    p line.chomp.split(/\t/)
+  loop do
+    while line = input.gets
+      p line.chomp.split(/\t/)
+      (sleep(1); next) if line =~ /EOS/
+    end
   end
 end
