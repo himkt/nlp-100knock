4c90606f47161181c65f43f0e0411fcdb2e22abb
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-03-29 01:25:30 +0900

    add 30

--- 4/30.rb
+++ 4/30.rb
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 require 'natto'
+list = Array.new
 
 # EOSまでで一行
 open('../data/neko.txt.mecab', 'r') do |input|
-  loop do
-    while line = input.gets
-      p line.chomp.split(/\t/)
-      (sleep(1); next) if line =~ /EOS/
+  sentense = Array.new
+  while line = input.gets
+    if line =~ /EOS/
+      list.push(sentense) unless sentense.empty?
+      sentense = Array.new
+      next 
     end
+    item = line.chomp.split(/\t/)
+    surface = item[0]
+    block = item[1].split(',')
+    sentense.push({:surface=>surface, :base=>block[6], :pos=>block[0], :pos1=>block[1]})
   end
 end
+
+p list.first(3)
+p list.size
