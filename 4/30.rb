559a3cd15a3bcdbe194d8d325fdeff60f2376fd7
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-03-29 17:09:58 +0900

    add 31

--- 4/30.rb
+++ 4/30.rb
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 require 'natto'
-list = Array.new
 
-# EOSまでで一行
-open('../data/neko.txt.mecab', 'r') do |input|
-  sentense = Array.new
-  while line = input.gets
-    if line =~ /EOS/
-      list.push(sentense) unless sentense.empty?
-      sentense = Array.new
-      next 
+def morphize
+  list = Array.new
+  # EOSまでで一行
+  open('../data/neko.txt.mecab', 'r') do |input|
+    sentense = Array.new
+    while line = input.gets
+      if line =~ /EOS/
+        list.push(sentense) unless sentense.empty?
+        sentense = Array.new
+        next 
+      end
+      item = line.chomp.split(/\t/)
+      surface = item[0]
+      block = item[1].split(',')
+      sentense.push({:surface=>surface, :base=>block[6], :pos=>block[0], :pos1=>block[1]})
     end
-    item = line.chomp.split(/\t/)
-    surface = item[0]
-    block = item[1].split(',')
-    sentense.push({:surface=>surface, :base=>block[6], :pos=>block[0], :pos1=>block[1]})
   end
+  return list
 end
 
-p list.first(3)
-p list.size
+# list = morphize
+
+# p list.first(3)
+# p list.size
