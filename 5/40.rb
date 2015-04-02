e16363fb50b53f5118f3238fcd5c7774dd6ab9e1
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-04-02 12:34:31 +0900

    update 40 but it is inserted break in code because of so long executing time

--- 5/40.rb
+++ 5/40.rb
@@ -5,24 +5,36 @@ class Morph
   def initialize(word)
     @natto = Natto::MeCab.new
     @word = word
-    p return_morpheme(word) 
+    return_token(word).split(/\t|,/).each_with_index do |item, index|
+      next unless index == 0 || index == 1 || index == 2 || index == 7
+      @surface = item if index == 0
+      @pos1 = item if index == 1
+      @pos2 = item if index == 2
+      @base = item if index == 7
+    end #unless word =~ /^EOS/
   end
-  def return_morpheme(word)#,arr)
-    if word =~ /^EOS/
-      return 'EOS'
-    else
-      @natto.parse(word).split(/\n/).each do |item|
-        return item unless item =~ /^EOS/
-      end
+end
+def return_token(word)#,arr)
+  if word =~ /^EOS/
+    return 'EOS'
+  else
+    @natto.parse(word).split(/\n/).each do |item|
+      return item unless item =~ /^EOS/
     end
   end
 end
 
 
+list = []
+
 open('../data/neko.txt.cabocha', 'r') do |input|
+  arr = []
   while word = input.gets
     word = word.chomp.gsub(/\-|D|\s|\|/, '')
-    Morph.new(word) 
+    break if list.size == 3
+    (list.push(arr); arr = []) if word =~ /^EOS/
+    arr.push(Morph.new(word))
   end
 end
 
+p list.pop
