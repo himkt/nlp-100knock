8095eb83ed28e29d61a5fa4f802088ade69034a6
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-04-02 17:49:02 +0900

    remake 40 from the beginning

--- 5/40.rb
+++ 5/40.rb
@@ -2,50 +2,34 @@
 require 'natto'
 
 class Morph
-  def initialize(word)
-    @natto = Natto::MeCab.new
-    @word = word
-    return_token(word).split(/\n/).each do |block|
-      #p item unless item =~ /^EOS|^D|-/
-      block.split(/,/).each_with_index do |item, index|
-      next unless index == 0 || index == 1 || index == 2 || index == 7
-      @surface = item if index == 0
-      @pos1 = item if index == 1
-      @pos2 = item if index == 2
-      @base = item if index == 7
-    end unless block =~ /^EOS|^D|-/
+  def initialize(morph)
+    @surface, block = morph.split(/\t/)
+    parse(block)
+  end
+  def parse(block)
+    block.split(/,/).each_with_index do |item, index|
+      if index == 0 || index == 1 || index == 6
+        @pos1 = item if index == 0
+        @pos2 = item if index == 1
+        @base = item if index == 6
+      end
     end
-=begin
-    p ii
-=end
-  end
-#  end
-
-end
-def return_token(word)#,arr)
-  if word =~ /^EOS/
-    return 'EOS'
-  else
-    return @natto.parse(word)#.split(/\n/).each do |item|
-      #p item
-      #return item unless item =~ /^EOS/
-   # end
   end
 end
 
+natto = Natto::MeCab.new
 
 list = []
 
 open('../data/neko.txt.cabocha', 'r') do |input|
   arr = []
-  while word = input.gets
-    word = word.chomp#.gsub(/\-|D|\s|\|/, '')
-    break if list.size == 3
-    (list.push(arr); arr = []) if word =~ /^EOS/
-    p word
-    arr.push(Morph.new(word))
-    p arr
+  while line = input.gets
+    line = line.chomp.gsub(/\-|D|\s|\|/, '')
+    natto.parse(line).split(/\n/).each do |morph|
+      (list.push(arr) if arr.size != 0; arr = []; next) if morph =~ /^EOS/
+      arr.push(Morph.new(morph))
+    end
   end
 end
 
-p list.pop
+p list[3]
