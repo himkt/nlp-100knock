dae6858ac55b887281915780e253bc74ad5869f0
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2015-04-03 15:48:43 +0900

    add 50 but have not done yet

---
+++ 6/50.rb
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+open('../data/nlp.txt', 'r') do |input|
+  text = input.read
+  text.split(/\.(?:\n|\s)/).each do |sentense|
+    p sentense
+  end
+=begin
+  while line = input.gets
+    line.chomp!
+    if line == ''
+      sleep(1)
+    else
+      if line =~ /^[A-Z]/
+        sleep(1)
+        line.split(/\./).each do |item|
+          p item
+          sleep(1)
+        end
+      else
+        line.split(/\./).each do |item|
+          p item
+          sleep(1)
+        end
+      end
+    end
+  end
+=end
+end
