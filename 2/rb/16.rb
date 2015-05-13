c3d668087fa429ac3981ea0f59487617a21b039d
himkt <himkt@klis.tsukuba.ac.jp>
2015-05-13 16:02:32 +0900

    fix #2

--- 2/rb/16.rb
+++ 2/rb/16.rb
@@ -7,22 +7,15 @@
 =end
 
 N = ARGV[0].to_i
-str = ''
-
-open('../../data/hightemp.txt', 'r') do |input|
-  while line = input.gets
-    str += line
-  end
-end
-
 i = 1
-arr = str.split("\n")
 
-N.times do
-  # floor: 切り捨て, ceil: 切り上げ, round: 四捨五入
-  (arr.size.to_f / N).ceil.times do
-    open("../../data/2_16/16-out-#{i}.txt", 'w') do |output|
-      output.print "#{arr.shift}\n"
+item = File.read('../../data/hightemp.txt').split(/\n/)
+
+while item != []
+  open("../../data/2_16/16-out-#{i}.txt",'w') do |output|
+    N.times do
+      break if item == []
+      output.print "#{item.pop}\n"
     end
   end
   i += 1
