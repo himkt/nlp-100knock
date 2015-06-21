b4256d06680dfe32666443d1d3070e3e3ca06624
himkt <himkt@klis.tsukuba.ac.jp>
2015-06-21 23:18:46 +0900

    comment: 24
    update: 24

--- 3/24.rb
+++ 3/24.rb
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
+=begin
+記事から参照されているメディアファイルをすべて抜き出せ．
+=end
+
 open('../data/uk.json', 'r') do |input|
   while line = input.gets
     line.split('\n').each do |item|
       if match = item.match(/(File:.*?)\|/) || match = item.match(/(ファイル:.*?)\|/)
-        p match[1]
+        puts match[1]
       end
     end
   end
