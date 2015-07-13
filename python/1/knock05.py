6d9aa084882c94418ccf151e0626574ef1ab033b
himkt <himkt@klis.tsukuba.ac.jp>
2015-07-13 20:44:20 +0900

    solve: 06
    fix: 05

--- python/1/knock05.py
+++ python/1/knock05.py
@@ -10,9 +10,6 @@ n-gramを作る関数を作成せよ．
 文字bi-gramを得よ．
 '''
 
-n = raw_input("n: ").decode("utf-8")
-string = raw_input("string: ").decode('utf-8')
-mode = raw_input("mode: ").decode('utf-8')
 
 
 def ngram(n, mode, string):
@@ -25,9 +22,16 @@ def ngram(n, mode, string):
   ngram_list = []
   for i in xrange(len(feature_list)):
     if len(feature_list) > i+1:
-      ngram_list.append("%s %s" % (feature_list[i], feature_list[i+1]))
+      ngram_list.append("%s%s" % (feature_list[i], feature_list[i+1]))
 
   return ngram_list
 
 
-print ngram(n, mode, string)
+if __name__ == '__main__':
+  # n = raw_input("n: ").decode("utf-8")
+  n = 2
+  string = "I am an NLPer"
+  # string = raw_input("string: ").decode('utf-8')
+  # mode = raw_input("mode: ").decode('utf-8')
+  print ngram(n, 'word', string)
+  print ngram(n, 'char', string)
