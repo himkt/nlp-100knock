1694e7e0cf014f9da33b1461ee8b6518bec9890c
himkt <himkt@klis.tsukuba.ac.jp>
2015-07-21 17:35:03 +0900

    fix: 30

--- 4/knock30.py
+++ 4/knock30.py
@@ -19,18 +19,27 @@
 def knock30():
 
     sentense_morph_list = []
-    morph_mapping = {}
+    morph_mapping_list = []
 
     for line in open('../data/neko.mecab', 'r'):
         line = line.rstrip()
     
         if line == 'EOS':
-            sentense_morph_list.append(morph_mapping)
-            morph_mapping = {}
+            sentense_morph_list.append(morph_mapping_list)
+            morph_mapping_list = []
             continue
     
         elements = line.split("\t")
-        morph_mapping[elements[1]] = elements[0]
+        morph_info = elements[1].split(",")
+    
+        morph = {
+           'surface' : elements[0],
+            'base'   : morph_info[6],
+            'pos'    : morph_info[0],
+            'pos1'   : morph_info[1]
+        }
+
+        morph_mapping_list.append(morph)
     
     return sentense_morph_list
 
