5505dde19c72faaaba146fcb367f757cc094af1d
himkt <himkt@klis.tsukuba.ac.jp>
2015-07-21 23:02:02 +0900

    solve: 34

---
+++ 4/knock34.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @author = himkt
+# @create = 2015/07/21
+
+'''
+34. 「AのB」
+
+2つの名詞が「の」で連結されている名詞句を抽出せよ．
+'''
+
+import knock30
+
+
+def extract_noun_phrase():
+
+    return True
+
+sentense_morph_list = knock30.knock30()
+
+for morph_list in sentense_morph_list:
+
+    for index in xrange(len(morph_list)):
+        
+        try:
+            
+            cond1 = morph_list[index+1]['surface'] == 'の'
+            cond2 = morph_list[index]['pos']   == '名詞'
+            cond3 = morph_list[index+2]['pos'] == '名詞'
+
+            if cond1 and cond2 and cond3:
+
+                print "%s%s%s" % (morph_list[index]['surface'], morph_list[index+1]['surface'], morph_list[index+2]['surface'])
+
+        except:
+
+            break
+
