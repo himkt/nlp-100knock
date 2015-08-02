5fc4ad1fc91579b46c8c383e5fab22679db52d36
himkt <himkt@klis.tsukuba.ac.jp>
2015-08-02 16:24:46 +0900

    modify: 54
    nest loops

--- 6/knock54.py
+++ 6/knock54.py
@@ -11,10 +11,21 @@ Stanford Core NLPの解析結果XMLを読み込み，
 
 import xml.etree.ElementTree as ET
 
-tree      = ET.parse('../data/nlp.xml')
-root      = tree.getroot()
-tokens    = root.findall("./document/sentences/sentence/tokens/token")
+def knock54():
 
-for token in tokens:
-    element = [item.text for item in token if item.tag in ['word', 'lemma', 'POS']]
-    print "\t".join(element)
+    tree      = ET.parse('../data/nlp.xml')
+    root      = tree.getroot()
+    tokens    = root.findall("./document/sentences/sentence/tokens/token")
+    
+    return [[item.text for item in token if item.tag in ['word', 'lemma', 'POS']] for token in tokens]
+
+    '''
+    for token in tokens:
+        element = [item.text for item in token if item.tag in ['word', 'lemma', 'POS']]
+        print "\t".join(element)
+    '''
+
+if __name__ == '__main__':
+    results = knock54()
+    for result in results:
+        print "\t".join(result)
