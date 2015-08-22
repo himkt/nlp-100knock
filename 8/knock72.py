3e677111c7e55535ad1ec32bdb42c3bd19d175cd
himkt <himkt@klis.tsukuba.ac.jp>
2015-08-22 14:51:26 +0900

    knock72: Update

--- 8/knock72.py
+++ 8/knock72.py
@@ -10,26 +10,26 @@
 '''
 
 import re
+import numpy
 from nltk.corpus import stopwords
 from stemming.porter2 import stem
 
 def knock72():
 
-    features = {'-1':[], '+1':[]}
+    features = []
     
     for line in open('../data/sentiment.txt', 'r'):
         label = line[:2]
         words = [word.lower() for word in re.sub("[\.\,\!\?;\:\(\)\[\]\'\"]$", '', line[3:].rstrip()).split()]
         for word in words:
-            features[label].append(stem(word))
+            features.append([label,stem(word)])
 
-    return features
+    return numpy.array(features)
 
 
 if __name__ == '__main__':
 
     features = knock72()
     
-    print len(features['+1'])
-    
-    print len(features['-1'])
+    print len([feature for feature in features if feature[0] == '-1'])
+    print len([feature for feature in features if feature[0] == '+1'])
