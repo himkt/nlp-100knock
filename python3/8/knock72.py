bc4d79b47bc71f0c405b96f03d9afb9b5e935d2d
himkt <himkt@klis.tsukuba.ac.jp>
2016-02-25 13:08:45 +0900

    Create notebook

--- python3/8/knock72.py
+++ python3/8/knock72.py
@@ -12,14 +12,19 @@ def dictionalize(features):
     return Dictionary(list(map(lambda x: x.split(' '), features)))
 
 
-def extract_features(features, stopwords):
+def extract_features(features, stopwords, dictionary):
     result = {'+1': list(), '-1': list()}
     corpus = list()
 
     for feature in features:
         label = feature[0:2]
         sentence = feature[2:]
-        feature_vec = Counter([stem(word) for word in sentence.split(' ') if not contain_stop(stopwords, stem(word))])
+        counter = Counter([stem(word) for word in sentence.split(' ') if not contain_stop(stopwords, stem(word))])
+
+        for feature, value in counter.items():
+            print(dictionary[feature])
+            print(value)
+            return 0
 
         corpus += list(feature_vec.keys())
         result[label].append(feature_vec)
@@ -32,6 +37,10 @@ if __name__ == '__main__':
     dictionary = dictionalize(features)
 
     stopwords = stopwords.words('english') + [',', '.', ';', ':', '']
-    filtered_features = extract_features(features, stopwords)
+    
+    print (dir(dictionary.token2id("I")))
+
+    filtered_features = extract_features(features, stopwords, dictionary)
 
     print(dictionary)
+    print(filtered_features)
