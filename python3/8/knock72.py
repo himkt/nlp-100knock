c791b0e8a3ff5eeee8fc9fdcf9df9687fc60f87d
himkt <himkt@klis.tsukuba.ac.jp>
2016-02-27 11:37:45 +0900

    Solve 72

--- python3/8/knock72.py
+++ python3/8/knock72.py
@@ -1,46 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from knock70 import init
-from knock71 import contain_stop
-from nltk.corpus import stopwords
-from stemming.porter2 import stem
-from collections import Counter
-from gensim.corpora import Dictionary
+from sklearn.feature_extraction.text import CountVectorizer
+from numpy import array
 
 
-def dictionalize(features):
-    return Dictionary(list(map(lambda x: x.split(' '), features)))
-
-
-def extract_features(features, stopwords, dictionary):
-    result = {'+1': list(), '-1': list()}
-    corpus = list()
-
+def vectorize(features):
+    y = list()
+    X = list()
     for feature in features:
-        label = feature[0:2]
-        sentence = feature[2:]
-        counter = Counter([stem(word) for word in sentence.split(' ') if not contain_stop(stopwords, stem(word))])
+        y.append(1 if feature[0:2] == '+1' else -1)
+        X.append(feature[3:])
 
-        for feature, value in counter.items():
-            print(dictionary[feature])
-            print(value)
-            return 0
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)
+    y = array(y)
 
-        corpus += list(feature_vec.keys())
-        result[label].append(feature_vec)
-
-    return (result, set(corpus))
-
+    return (X, y)
 
 if __name__ == '__main__':
     features = init()
-    dictionary = dictionalize(features)
-
-    stopwords = stopwords.words('english') + [',', '.', ';', ':', '']
-    
-    print (dir(dictionary.token2id("I")))
-
-    filtered_features = extract_features(features, stopwords, dictionary)
-
-    print(dictionary)
-    print(filtered_features)
+    X, y = vectorize(features)
+    print(X, y)
