33a7dcfbe35304bdbc8db8f57e795e5b5c9d0b7e
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-25 15:19:56 +0900

    Cleanup

---
+++ python3/chapter5/knock46.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+"""
+46. 動詞の格フレーム情報の抽出
+
+45のプログラムを改変し，述語と格パターンに続けて項（述語に係っている文節そのもの）をタブ区切り形式で出力せよ．45の仕様に加えて，以下の仕様を満たすようにせよ．
+
+項は述語に係っている文節の単語列とする（末尾の助詞を取り除く必要はない）
+述語に係る文節が複数あるときは，
+助詞と同一の基準・順序でスペース区切りで並べる
+「吾輩はここで始めて人間というものを見た」という例文（neko.txt.cabochaの8文目）を考える．
+この文は「始める」と「見る」の２つの動詞を含み，
+「始める」に係る文節は「ここで」，
+「見る」に係る文節は「吾輩は」と「ものを」と解析された場合は，
+次のような出力になるはずである．
+"""
+
+from knock41 import parse
+
+
+if __name__ == '__main__':
+    document = parse()
+    # sentence = document[7]
+
+    for sentence in document:
+        for chunk in sentence:
+            if '動詞' in [m.pos for m in chunk.morphs]:
+
+                # We treat least left verb as a predicate
+                predicate = [m.base for m in chunk.morphs if m.pos == '動詞'][0]
+
+                case_list = []
+                for src in chunk.srcs:
+                    if '助詞' in [m.pos for m in sentence[src].morphs]:
+                        case = [m.base for m in sentence[
+                            src].morphs if m.pos == '助詞'][-1]
+                        case_list.append(case)
+
+                if len(case_list) > 0:
+                    case_list.sort()
+                    print('%s\t%s' % (predicate, ' '.join(case_list)))
