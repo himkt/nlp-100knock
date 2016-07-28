d083a5bda444dba14eecf6affc33b017bbb5c04e
himkt <himkt@klis.tsukuba.ac.jp>
2016-07-28 20:37:43 +0900

    [General] Update init.sh for knock91

--- init.sh
+++ init.sh
@@ -26,3 +26,8 @@ bunzip2 data/enwiki-20150112-400-r10-105752.txt.bz2
 
 wget http://www.cl.ecei.tohoku.ac.jp/nlp100/data/enwiki-20150112-400-r100-10576.txt.bz2 -P data
 bunzip2 data/enwiki-20150112-400-r100-10576.txt.bz2
+
+wget https://storage.googleapis.com/google-code-archive-source/v2/code.google.com/word2vec/source-archive.zip -P data
+unzip data/source-archive.zip -d data
+mv data/word2vec/trunk/questions-words.txt data/questions-words.txt
+rm -rf data/word2vec data/source-archive.zip*
