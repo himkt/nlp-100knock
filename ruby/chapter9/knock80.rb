671d4f9269abd0a9f9c617b32bbed67e65aaf837
himkt <himkt@klis.tsukuba.ac.jp>
2016-07-28 13:59:01 +0900

    [Ruby] Add knock80

---
+++ ruby/chapter9/knock80.rb
@@ -0,0 +1,17 @@
+doc = []
+pat = "[#{["\\", "\\.", "\\,", "\\!", "\\?", "\\;", "\\:", "\\(", "\\)", "\\[", "\\]", "\\'", "\""].join()}]"
+
+File.foreach('../../data/enwiki-20150112-400-r100-10576.txt').with_index do |line, index|
+  p index if index % 50000 == 0
+  sentence = line.chomp.split.map {|token|
+    token.
+      sub(/^#{pat}/, '').
+      sub(/#{pat}$/, '').
+      gsub(/#{pat}/, '') # ズル
+    }.
+    reject{|token| token.empty?}
+  
+  doc << sentence.join("\s") if sentence.size > 5
+end
+
+Marshal.dump(doc, open('../../data/knock80.dump', 'wb'))
