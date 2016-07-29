554fafd3096689219b6ff99aad150f80921518bd
himkt <himkt@klis.tsukuba.ac.jp>
2016-07-29 19:00:35 +0900

    [Ruby] Modify knock94

--- ruby/chapter10/knock94.rb
+++ ruby/chapter10/knock94.rb
@@ -13,12 +13,14 @@ if __FILE__ == $0
 
   results = []
   File.foreach('../../data/combined.tsv') do |line|
-    word1, word2, _ = line.split
+    word1, word2, score = line.split
+    score = score.to_f
+
     next unless token2id[word1] && token2id[word2]
     similarity = sim(word_matrix[token2id[word1], true], word_matrix[token2id[word2], true])
-    puts "#{word1}\t#{word2}\t#{similarity}"
-    results << [word1, word2, similarity]
+    puts "#{word1}\t#{word2}\t#{similarity}\t#{score}"
+    results << [word1, word2, similarity, score]
   end
 
-  Marshal.dump(results, open('../../data/knock95.dump', 'wb'))
+  Marshal.dump(results, open('../../data/knock94.dump', 'wb'))
 end
