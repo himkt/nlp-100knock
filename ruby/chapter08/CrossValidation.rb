829742a48ba1d7e42f5aac9bcdf6484b44acaf08
himkt <himkt@klis.tsukuba.ac.jp>
2016-07-29 13:36:10 +0900

    [Ruby] Implement KFolds

--- ruby/chapter08/CrossValidation.rb
+++ ruby/chapter08/CrossValidation.rb
@@ -11,4 +11,33 @@ module CrossValidation
 
     return [x[train_indices, true], y[train_indices, true], y[test_indices, true], y[test_indices, true]]
   end
+
+  class KFold
+    def initialize(n, n_folds, shuffle)
+      indices = (0...n).to_a
+      indices.shuffle! if shuffle
+
+      @indices = indices
+      @n_folds = n_folds
+    end
+
+    def create
+      groups_nfolds = @indices.each_slice((@indices.size.to_f / @n_folds).ceil).to_a
+      groups = []
+
+      @n_folds.times do |k|
+        validation_set = []
+        test_set = []
+
+        @n_folds.times do |j|
+          test_set += groups_nfolds[j] if k == j
+          validation_set += groups_nfolds[j] unless k == j
+        end
+
+        groups << [validation_set, test_set]
+      end
+
+      return groups
+    end
+  end
 end
